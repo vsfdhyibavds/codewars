7b526fc079c377e715913c032e945f32089cd76e
Eugene Wekesa <eugenco578@gmail.com>
2025-05-21 14:50:55 +0300

    practice

---
+++ xo.py
@@ -0,0 +1,15 @@
+# Check to see if a string has the same amount of 'x's and 'o's. The method must return a boolean and be case insensitive. The string can contain any char.
+
+# Examples input/output:
+
+# XO("ooxx") => true
+# XO("xooxx") => false
+# XO("ooxXm") => true
+# XO("zpzpzpp") => true // when no 'x' and 'o' is present should return true
+# XO("zzoo") => false
+
+def xo(s):
+    s = s.lower()
+    count_x = s.count('x')
+    count_o = s.count('o')
+    return count_x == count_o
