7f88e702145ff7e2309bbb54743d6c1e7e271b92
Eugene Wekesa <eugenco578@gmail.com>
2025-05-22 23:45:46 +0300

    narcissistic number

---
+++ Narcissistic_number.py
@@ -0,0 +1,17 @@
+# A Narcissistic Number (or Armstrong Number) is a positive number which is the sum of its own digits, each raised to the power of the number of digits in a given base. In this Kata, we will restrict ourselves to decimal (base 10).
+
+# For example, take 153 (3 digits), which is narcissistic:
+
+#     1^3 + 5^3 + 3^3 = 1 + 125 + 27 = 153
+# and 1652 (4 digits), which isn't:
+
+#     1^4 + 6^4 + 5^4 + 2^4 = 1 + 1296 + 625 + 16 = 1938
+# The Challenge:
+
+# Your code must return true or false (not 'true' and 'false') depending upon whether the given number is a Narcissistic number in base 10.
+
+def narcissistic(number):
+    str_num = str(number)
+    n = len(str_num)
+    total = sum(int(digit) ** n for digit in str_num)
+    return total == number
